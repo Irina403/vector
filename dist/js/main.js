a9f86e5b79d237737aec33b808aaf935fe51c075
Irina403 <92080770+Irina403@users.noreply.github.com>
2023-01-22 14:13:14 +0300

    22.01

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
         slidesToShow: 3,
         slidesToScroll: 1,
         dots: false,
+        autoplay: true,
         prevArrow: "<img src='i/icons/arrow-prev-black.svg' class='arrow-black prev3' alt='1'>",
         nextArrow: "<img src='i/icons/arrow-next-black.svg' class='arrow-black next3' alt='2'>",
         responsive: [{
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                     // arrows: false,
                     infinite: true,
@@ -62,3 +63,35 @@ $(document).ready(function() {
 
 
 });
+
+
+function raschitat() {
+    console.log('yes');
+    material = document.getElementById('material').value;
+    switch (material) {
+        case "pgs":
+            cena = 440;
+            break
+        case "pesok":
+            cena = 480;
+            break
+        case "grunt":
+            cena = 380;
+            break
+        default:
+            cena = 440;
+            break
+    }
+    volume = document.getElementById('volume').value;
+    distance = document.getElementById('distance').value;
+    if (volume == "") {
+        alert("Вы не указали объем");
+    } else if (distance == "") {
+        alert("Вы не указали расстояние");
+    } else {
+        // ploschad = parseFloat(shirina) * parseFloat(dlina);
+        // document.getElementById('ploschad').innerHTML = "Площадь равна: " + ploschad + " кв. м.";
+        stoimost = volume * distance;
+        document.getElementById('stoimost').innerHTML = "Стоимость равна: " + stoimost + " р.";
+    }
+}
